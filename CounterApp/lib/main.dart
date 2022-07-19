89a487c76e6b95b33ab9d3c36d7f0cb35b5d10de
dinhpv38 <phamdoanmanu38@gmail.com>
2022-07-19 20:31:40 +0700

    - Demo lib bloc with count app

--- CounterApp/lib/main.dart
+++ CounterApp/lib/main.dart
@@ -1,24 +1,144 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'blocs/counter_bloc.dart';
-import 'counter_page.dart';
 
 void main() {
-  runApp(MyApp());
+  BlocOverrides.runZoned(
+        () => runApp(const App()),
+    blocObserver: AppBlocObserver(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  //Now we need to install bloc
-  //We have 2 Events => Increase / Descrease
+/// Custom [BlocObserver] that observes all bloc and cubit state changes.
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    if (bloc is Cubit) print(change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+}
+
+class App extends StatelessWidget {
+  /// {@macro app}
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Increment / Decrement Counter App using Bloc',
-        home: BlocProvider<CounterBloc>(
-          create: (context) => CounterBloc(),
-          child: CounterPage(),
-        )
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: const AppView(),
     );
   }
 }
 
+class AppView extends StatelessWidget {
+  /// {@macro app_view}
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, ThemeData>(
+      builder: (_, theme) {
+        return MaterialApp(
+          theme: theme,
+          home: const CounterPage(),
+        );
+      },
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: const CounterView(),
+    );
+  }
+}
+
+class CounterView extends StatelessWidget {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        child: BlocBuilder<CounterBloc, int>(
+          builder: (context, count) {
+            return Text('$count', style: Theme.of(context).textTheme.headline1);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              context.read<CounterBloc>().add(CounterIncrementPressed());
+            },
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () {
+              context.read<CounterBloc>().add(CounterDecrementPressed());
+            },
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.brightness_6),
+            onPressed: () {
+              context.read<ThemeCubit>().toggleTheme();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+abstract class CounterEvent {}
+
+class CounterIncrementPressed extends CounterEvent {}
+
+class CounterDecrementPressed extends CounterEvent {}
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) => emit(state + 1));
+    on<CounterDecrementPressed>((event, emit) => emit(state - 1));
+  }
+}
+
+class ThemeCubit extends Cubit<ThemeData> {
+  ThemeCubit() : super(_lightTheme);
+
+  static final _lightTheme = ThemeData(
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.white,
+    ),
+    brightness: Brightness.light,
+  );
+
+  static final _darkTheme = ThemeData(
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.black,
+    ),
+    brightness: Brightness.dark,
+  );
+
+  void toggleTheme() {
+    emit(state.brightness == Brightness.dark ? _lightTheme : _darkTheme);
+  }
+}
