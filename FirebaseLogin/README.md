a06a7e02b2a779928b427adef91637921d44ace6
dinhpv38 <phamdoanmanu38@gmail.com>
2022-07-25 21:40:19 +0700

    - Done login firebase

--- FirebaseLogin/README.md
+++ FirebaseLogin/README.md
@@ -14,3 +14,8 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+Link tham khảo
+- https://blog.codemagic.io/firebase-authentication-google-sign-in-using-flutter/
+- https://kickertech.com/login-and-register-easily-with-flutter-using-firebase/
--- FirebaseLogin/lib/main.dart
+++ FirebaseLogin/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'Screens/Login_Screen.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,9 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SafeArea(
-          child: LoginPage()
-      ),
+      home:LoginScreen(),
     );
   }
 }
